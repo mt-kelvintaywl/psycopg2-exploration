cb03260ea646c64bcbc73a464b1eec87c9875082
Kelvin <ktay@moneytree.jp>
2021-08-06 15:02:19 +0900

    feat: run query

--- schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE comments (
-  id SERIAL PRIMARY KEY,
-  message TEXT NOT NULL
-);
-
-INSERT INTO comments (message) VALUES ('first blood!'), ('hello'), ('whut');
--- try.py
+++ try.py
@@ -3,11 +3,34 @@ import os
 import psycopg2 as pg
 
 
+def setup_tables(cursor):
+  # create table(s)
+  cursor.execute("""
+  CREATE TABLE comments (
+    id SERIAL PRIMARY KEY,
+    message TEXT NOT NULL
+  );""")
+
+  cursor.execute("""
+  INSERT INTO comments (message) VALUES ('first blood!'), ('hello'), ('whut');
+  """)
+
+
+def fancy_query(cursor):
+  qry = """
+  SELECT id, message
+  FROM comments
+  LIMIT %(limit)s
+  """
+  args = { "limit": 1 }
+  cursor.execute(qry, args)
+  print(cursor.fetchall())
+
 def main(db_url: str):
     conn = pg.connect(db_url)
-    with conn.cursor() as curr:
-        curr.execute(open("schema.sql", "r").read())
-
+    with conn.cursor() as cursor:
+        setup_tables(cursor)
+        fancy_query(cursor)
     conn.close()
 
 
